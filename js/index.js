0d521fef88b9bbd21938708c754a5f3e1953f015
Quinn Shannon <quinn.p.shannon@gmail.com>
2024-04-16 23:01:06 -0400

    Made some functions to do some work

---
+++ js/index.js
@@ -0,0 +1,148 @@
+// The provided course information.
+// const CourseInfo = {
+//     id: 999,
+//     name: "Introduction to JavaScript"
+//   };
+const CourseInfo = {
+    id: 451,
+    name: "Introduction to JavaScript"
+  }; //Default Data
+  
+  // The provided assignment group.
+  const AssignmentGroup = {
+    id: 12345,
+    name: "Fundamentals of JavaScript",
+    course_id: 451,
+    group_weight: 25,
+    assignments: [
+      {
+        id: 1,
+        name: "Declare a Variable",
+        due_at: "2023-01-25",
+        points_possible: 50
+      },
+      {
+        id: 2,
+        name: "Write a Function",
+        due_at: "2023-02-27",
+        points_possible: 150
+      },
+      {
+        id: 3,
+        name: "Code the World",
+        due_at: "3156-11-15",
+        points_possible: 500
+      }
+    ]
+  };
+  
+  // The provided learner submission data.
+  const LearnerSubmissions = [
+    {
+      learner_id: 125,
+      assignment_id: 1,
+      submission: {
+        submitted_at: "2023-01-25",
+        score: 47
+      }
+    },
+    {
+      learner_id: 125,
+      assignment_id: 2,
+      submission: {
+        submitted_at: "2023-02-12",
+        score: 150
+      }
+    },
+    {
+      learner_id: 125,
+      assignment_id: 3,
+      submission: {
+        submitted_at: "2023-01-25",
+        score: 400
+      }
+    },
+    {
+      learner_id: 132,
+      assignment_id: 1,
+      submission: {
+        submitted_at: "2023-01-24",
+        score: 39
+      }
+    },
+    {
+      learner_id: 132,
+      assignment_id: 2,
+      submission: {
+        submitted_at: "2023-03-07",
+        score: 140
+      }
+    }
+  ];
+  
+  function getLearnerData(course, ag, submissions) {
+    return isValidScore(LearnerSubmissions[0].submission.score,AssignmentGroup.assignments[0].points_possible)
+    // Here is an example of what we are looking for, but no math was done
+    // const result = [
+    //   {
+    //     id: 125,
+    //     avg: 0.985, // (47 + 150) / (50 + 150)
+    //     1: 0.94, // 47 / 50
+    //     2: 1.0 // 150 / 150
+    //   },
+    //   {
+    //     id: 132,
+    //     avg: 0.82, // (39 + 125) / (50 + 150)
+    //     1: 0.78, // 39 / 50
+    //     2: 0.833 // late: (140 - 15) / 150
+    //   }
+    // ];
+    return result;
+  }
+  try {
+  if (bouncer(CourseInfo.id,AssignmentGroup.course_id)){
+    throw new Error('Assignment Group is not part of the Course group!')
+  }
+  const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
+  
+  console.log(result);
+  }
+  catch (e) {
+    console.error(e);
+  }
+  function bouncer(base, compare){
+    return base !== compare;
+  }
+  function isValidScore(pts,total){
+    if(total===0) {
+      throw new Error('Cannot be worth Zero points');
+    }
+    if (typeof pts != "number"){
+      throw new Error('Points is not a Number Value!');  
+    }
+    if (typeof total != "number"){
+      throw new Error('Total is not a Number Value!');  
+    }
+    if (pts > total){
+      throw new Error('Not Valid score, No extra credit!');
+    }
+    return true;
+  }
+  function isDue(dueDate,submitDate){
+    // If the assignment is Due, return true
+    if (Date.valueOf(dueDate) <= Date.now()) return true;
+    return false;
+  }
+  function isLate(dueDate,submitDate){
+    // If the assignment is late, Return True
+    if (Date.valueOf(submitDate) > Date.valueOf(dueDate)) return true;
+    return false;
+  }
+  function splitLearner(array,learnID){
+    // Take the -array- of Number scores, make a new array,
+    // and split out one specific learner by -learnID-
+
+  }
+  function gradeLearner(){
+
+  }
